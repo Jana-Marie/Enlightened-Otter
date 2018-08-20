19bd308b9c0b691a333f1c3ebf181284781d936a
Jan--Henrik <janhenrik@janhenrik.org>
2018-08-20 10:11:32 +0200

    added license

--- Src/init_functions.c
+++ Src/init_functions.c
@@ -1,5 +1,22 @@
+/*
+ * Enlighted-Otter  -  Stm32f334 based mobile worklight.
+ * Copyright (C) 2018 Jan Henrik Hemsing
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "init_functions.h"
-#include "defines.h"
 
 ADC_HandleTypeDef hadc1;
 ADC_HandleTypeDef hadc2;
