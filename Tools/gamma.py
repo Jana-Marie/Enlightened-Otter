aaa2a26ebaa4239df62037da5603f347aba08890
Jan--Henrik <janhenrik@janhenrik.org>
2018-08-21 10:25:43 +0200

    fixed script

--- Tools/gamma.py
+++ Tools/gamma.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3.7
 import math
 
-gamma       = 2.8
+gamma       = 1.6
 outputRange = 1
 steps       = 1000
 
-print("float gamma[] = {",end='')
+print("float gamma[] = { // generated gamma correction table from Tools/gamma.py gamma value used: %.1f resolution: %.5f " % (gamma,outputRange/steps),end='')
 
-for i in range (0,steps,outputRange):
+for i in range (0,steps,1):
 
   if(i > 0):
     print(",",end='')
   if((i & 15) == 0):
     print()
-  print("\t%.4f" % round((math.pow(i / steps, gamma) * steps + 0.5)/(steps*outputRange),4),end='')
+  print("\t%.4f" % round((math.pow(i / steps, gamma) * steps + 0.5)/steps*outputRange,4),end='')
 
 print(" };")
 exit()
